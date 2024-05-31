025a2e89fd2e923cf02708b2b4fcc57b1f3de985
amosdinh <amos.dinh.2013@gmail.com>
2024-05-31 17:52:32 +0200

    cleanup

---
+++ utils.py
@@ -0,0 +1,11 @@
+import torch 
+import random 
+import numpy as np
+
+def fix_random(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
